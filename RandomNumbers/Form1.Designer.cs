c3508a562ac10b2335eabbddb3b22e62437979db
Kiril Spasov <spasov.kiril@gmail.com>
2022-11-03 12:40:48 +0200

    Add project files.

---
+++ RandomNumbers/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace RandomNumbers
+{
+    partial class FormRandomNumbers
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtText = new System.Windows.Forms.TextBox();
+            this.BtnCoinToss = new System.Windows.Forms.Button();
+            this.Btn4CoinsToss = new System.Windows.Forms.Button();
+            this.BtnDiceCounter = new System.Windows.Forms.Button();
+            this.BtnDiceWordsGame = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TxtText
+            // 
+            this.TxtText.Location = new System.Drawing.Point(463, 55);
+            this.TxtText.Multiline = true;
+            this.TxtText.Name = "TxtText";
+            this.TxtText.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.TxtText.Size = new System.Drawing.Size(265, 315);
+            this.TxtText.TabIndex = 1;
+            // 
+            // BtnCoinToss
+            // 
+            this.BtnCoinToss.Location = new System.Drawing.Point(57, 45);
+            this.BtnCoinToss.Name = "BtnCoinToss";
+            this.BtnCoinToss.Size = new System.Drawing.Size(187, 43);
+            this.BtnCoinToss.TabIndex = 3;
+            this.BtnCoinToss.Text = "Coin Toss";
+            this.BtnCoinToss.UseVisualStyleBackColor = true;
+            this.BtnCoinToss.Click += new System.EventHandler(this.BtnCoinToss_Click);
+            // 
+            // Btn4CoinsToss
+            // 
+            this.Btn4CoinsToss.Location = new System.Drawing.Point(55, 94);
+            this.Btn4CoinsToss.Name = "Btn4CoinsToss";
+            this.Btn4CoinsToss.Size = new System.Drawing.Size(186, 42);
+            this.Btn4CoinsToss.TabIndex = 4;
+            this.Btn4CoinsToss.Text = "4 Coins Toss";
+            this.Btn4CoinsToss.UseVisualStyleBackColor = true;
+            this.Btn4CoinsToss.Click += new System.EventHandler(this.Btn4CoinsToss_Click);
+            // 
+            // BtnDiceCounter
+            // 
+            this.BtnDiceCounter.Location = new System.Drawing.Point(57, 142);
+            this.BtnDiceCounter.Name = "BtnDiceCounter";
+            this.BtnDiceCounter.Size = new System.Drawing.Size(184, 41);
+            this.BtnDiceCounter.TabIndex = 5;
+            this.BtnDiceCounter.Text = "Dice Counter";
+            this.BtnDiceCounter.UseVisualStyleBackColor = true;
+            this.BtnDiceCounter.Click += new System.EventHandler(this.BtnDiceCounter_Click);
+            // 
+            // BtnDiceWordsGame
+            // 
+            this.BtnDiceWordsGame.Location = new System.Drawing.Point(55, 189);
+            this.BtnDiceWordsGame.Name = "BtnDiceWordsGame";
+            this.BtnDiceWordsGame.Size = new System.Drawing.Size(184, 41);
+            this.BtnDiceWordsGame.TabIndex = 6;
+            this.BtnDiceWordsGame.Text = "Dice Words Game";
+            this.BtnDiceWordsGame.UseVisualStyleBackColor = true;
+            this.BtnDiceWordsGame.Click += new System.EventHandler(this.BtnDiceWordsGame_Click);
+            // 
+            // FormRandomNumbers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.BtnDiceWordsGame);
+            this.Controls.Add(this.BtnDiceCounter);
+            this.Controls.Add(this.Btn4CoinsToss);
+            this.Controls.Add(this.BtnCoinToss);
+            this.Controls.Add(this.TxtText);
+            this.Name = "FormRandomNumbers";
+            this.Text = "Random Numbers";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox TxtText;
+        private System.Windows.Forms.Button BtnCoinToss;
+        private System.Windows.Forms.Button Btn4CoinsToss;
+        private System.Windows.Forms.Button BtnDiceCounter;
+        private System.Windows.Forms.Button BtnDiceWordsGame;
+    }
+}
+
---
+++ RandomNumbers/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandomNumbers
+{
+    public partial class FormRandomNumbers : Form
+    {
+        public FormRandomNumbers()
+        {
+            InitializeComponent();
+        }
+
+
+        //Write a program which determines how many times a toss of 100 coins
+        //contains 47 heads if the experiment is performed 50 times.
+        private void BtnCoinToss_Click(object sender, EventArgs e)
+        {
+            TxtText.Clear();
+            Random r = new Random();
+
+            int heads = 1;
+            int toss;
+            int headsCount = 0;
+            int occurance = 0;
+
+            for (int i = 1; i <= 50; i++)
+            {
+                headsCount = 0;
+
+                for (int j = 1; j <= 100; j++)
+                {
+                    toss = r.Next(1, 3);
+                    if (toss == 1)
+                        headsCount++;
+                }
+
+                if (headsCount == 47)
+                    occurance++;
+            }
+
+            TxtText.Text = "There were " + occurance + " occurances of 47 heads out of 100";
+        }
+
+        //Write a program which will simulate the tossing of 4 fair coins, 50 times.
+        //Display the result of each toss of four on a separate line.
+        //If exactly 3 heads occur on a given toss, follow the line with an asterisk (*).
+        //Example of output:
+        //HTTH
+        //HHTH*
+        //THTT
+        //Count the number of times exactly 3 heads occur in the 50 tosses.
+        private void Btn4CoinsToss_Click(object sender, EventArgs e)
+        {
+            TxtText.Clear();
+            Random r = new Random();
+            int toss;
+            int headsCount;
+            int count3Heads = 0;
+
+            for (int i = 1; i <= 50; i++)
+            {
+                headsCount = 0;
+                for (int j = 1; j <= 4; j++)
+                {
+                    toss = r.Next(1, 3);
+                    if (toss == 1)
+                    {
+                        TxtText.Text += "H";
+                        headsCount++;
+                    }
+                    else
+                    {
+                        TxtText.Text += "T";
+                    }
+                }
+
+                if (headsCount == 3)
+                {
+                    TxtText.Text += "*";
+                    count3Heads++;
+                }
+                    
+
+                TxtText.Text += Environment.NewLine;
+            }
+
+            TxtText.Text += "There were 3 heads " + count3Heads + " times in 50 games.";
+        }
+
+
+        //Write a program that simulates 15 rolls of a die and then prints out the number of times
+        //each number is generated.
+        private void BtnDiceCounter_Click(object sender, EventArgs e)
+        {
+            TxtText.Clear();
+            Random r = new Random();
+
+            int toss;
+            int[] numbersCounter = new int[7];
+
+            for (int i = 1; i <= 15; i++)
+            {
+                toss = r.Next(1, 7);
+                numbersCounter[toss] += 1;
+            }
+
+            for (int i = 1; i <= 6; i++)
+            {
+                TxtText.Text += i + " got tossed " + numbersCounter[i] + " times" + Environment.NewLine;
+            }
+        }
+
+        //Write a program that simulates the roll of two die. Instead of printing the numbers
+        //print the word representations of them and also the word representations of their sum.
+        private void BtnDiceWordsGame_Click(object sender, EventArgs e)
+        {
+            TxtText.Clear();
+            TxtText.Text = "Dice 1 \t Dice 2 \t Sum " + Environment.NewLine;
+            Random r = new Random();
+            int dice1, dice2;
+            int sum;
+            string[] wordsOfNumbers = new string[13]
+            { "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve"};
+
+
+            for (int i = 1; i <= 2; i++)
+            {
+                sum = 0;
+                dice1 = r.Next(1, 7);
+                dice2 = r.Next(1, 7);
+                sum = dice1 + dice2;
+
+                TxtText.Text += wordsOfNumbers[dice1] + "\t" + wordsOfNumbers[dice2] + "\t" + wordsOfNumbers[sum] + Environment.NewLine;
+            }
+        }
+    }
+}
